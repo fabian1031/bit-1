65fbf5e3a2023481b7769d13dc184e92b1015537
fabian <lugia759@gmail.com>
2025-05-05 21:52:04 -0500

    update form

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const filterButtons = document.querySelectorAll('.filter-btn');
 const items = document.querySelectorAll('.portfolio-item');
 
@@ -13,4 +15,30 @@ filterButtons.forEach(button => {
       }
     });
   });
+});
+
+const $form = document.getElementById("form")
+$form.addEventListener("submit", (event) => {
+  event.preventDefault();
+const name = $form.name.value;
+const lastname = $form.lastname.value;
+const email = $form.email.value;
+const country = $form.country.value;
+const subject = $form.subject.value;
+
+
+
+const result = `Hola ${name} ${lastname}, 
+
+Gracias por tu mensaje. Hemos recibido tu consulta sobre "${subject}" y pronto te responderemos. 
+
+Hemos registrado tu correo electrónico: ${email}, y sabemos que nos contactas desde ${country}. 
+
+¡Estaremos en contacto pronto!`;
+
+console.log(result);
+
+
+  alert(result);
+  $form.reset();
 });
